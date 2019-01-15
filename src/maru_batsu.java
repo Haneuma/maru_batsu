0a9805b73c69fcf4fe76eae002649566856a745a
s18sc04 <s18sc04@202B01.jjc-net.local>
2019-01-15 12:57:29 +0900

    20190115

--- src/maru_batsu.java
+++ src/maru_batsu.java
@@ -4,19 +4,23 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class maru_batsu extends Application {
 
-	final Canvas canvas = new Canvas(300, 400);
+	final Canvas canvas = new Canvas(320, 320);
 	GraphicsContext gc = canvas.getGraphicsContext2D();
 	int i = 0;
-	int[] XStack = new int[19];
-	int[] YStack = new int[19];
-	boolean resault;
+	int XDigit;
+	int YDigit;
+	int Stack[][] = new int[3][3];
+	int type[][] = new int[3][3];
+	Label result = new Label("結果がここに表示されます");
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
@@ -30,31 +34,80 @@ public class maru_batsu extends Application {
 		stage.setHeight(400);
 		stage.setTitle("○×ゲーム");
 
-		BorderPane root = new BorderPane();
+		BorderPane border = new BorderPane();
 
-		root.setOnMouseClicked(event -> Drawimg(event, i++));
+		border.setOnMouseClicked(event -> Drawimg(event, i, stage));
 
-		root.setCenter(canvas);
+		border.setCenter(canvas);
+		VBox root = new VBox();
+		root.setSpacing(10);
+		root.getChildren().addAll(border, result);
 
 		stage.setScene(new Scene(root));
 		stage.show();
 
 	}
 
-	void Drawimg(MouseEvent event, int count) {
+	void Drawimg(MouseEvent event, int count, Stage stage) {
 		int X = (int) (event.getSceneX() / 100) * 100; //座標指定X
 		int Y = (int) (event.getSceneY() / 100) * 100; //座標指定Y
+		XDigit = X / 100;
+		YDigit = Y / 100;
 
-		if (count % 2 == 0) {
+		if (Stack[XDigit][YDigit] == 1) {
 
-			Image img = new Image(Paths.get("image/mark_maru_s.png").toUri().toString());
-			gc.drawImage(img, X, Y);
+			i--;
+		} else if (count % 2 == 0) {
 
+			Image imgm = new Image(Paths.get("image/mark_maru_s.png").toUri().toString());
+			gc.drawImage(imgm, X, Y);
+			type[XDigit][YDigit] = 8;
 		} else if (count % 2 == 1) {
 
-			Image img = new Image(Paths.get("image/mark_batsu_s.png").toUri().toString());
-			gc.drawImage(img, X, Y);
+			Image imgb = new Image(Paths.get("image/mark_batsu_s.png").toUri().toString());
+			gc.drawImage(imgb, X, Y);
+			type[XDigit][YDigit] = 9;
+		}
 
+		Stack[XDigit][YDigit] = 1;
+
+		i++;
+			if (i == 9)
+			result(stage);
+	}
+
+	void result(Stage stage) {
+		// TODO 自動生成されたメソッド・スタブ
+		int j = 0;
+		int k = 0;
+		for (j = 0; j < 3; j++) {
+
+			if ((type[j][k] == 8) && (type[j][k + 1] == 8) && (type[j][k + 2] == 8)) {
+				result.setText("○の勝ち");
+			} else if ((type[j][k] == 9) && (type[j][k + 1] == 9) && (type[j][k + 2] == 9)) {
+				result.setText("×の勝ち");
+			}
+		}
+
+		for (k = 0; k < 3; k++) {
+
+			if ((type[j][k] == 8) && (type[j + 1][k] == 8) && (type[j + 2][k] == 8)) {
+				result.setText("○の勝ち");
+			} else if ((type[j][k] == 9) && (type[j + 1][k] == 9) && (type[j + 2][k] == 9)) {
+				result.setText("×の勝ち");
+			}
+		}
+
+		if ((type[0][0] == 8) && (type[1][1] == 8) && (type[2][2] == 8)) {
+			result.setText("○の勝ち");
+		} else if ((type[0][0] == 9) && (type[1][1] == 9) && (type[2][2] == 9)) {
+			result.setText("×の勝ち");
+		}
+
+		if ((type[0][2] == 8) && (type[1][1] == 8) && (type[2][0] == 8)) {
+			result.setText("○の勝ち");
+		} else if ((type[0][2] == 9) && (type[1][1] == 9) && (type[2][0] == 9)) {
+			result.setText("×の勝ち");
 		}
 
 	}
